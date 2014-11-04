acc7c31b58c2b1579fadfbd8eb4f64475b7c5040
Ethan <edff1999@gmail.com>
2014-11-03 17:57:35 -0800

    megaman
    sort of started

--- gist976247a1e61dd7187b2c-88f4a6ed63ea14378168b843e473f75f4fa4e006/game.js
+++ gist976247a1e61dd7187b2c-88f4a6ed63ea14378168b843e473f75f4fa4e006/game.js
@@ -5,33 +5,58 @@ var main = {
     // load all the images and sounds
     preload: function() {
         game.stage.backgroundColor = '#71c5cf';	  	
-        game.load.image('missionbit', REPLACEME);
+        game.load.image('missionbit', "Goku_(PeePee).png");
+                game.load.image('poop', "Virginia.png");
     },
 
     // set up the game
     create: function() {
-        // create the player using the missionbit image and place it at (100, 245)
-        this.player = game.add.sprite(100, 245, 'missionbit');
-    },
+       game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.player = game.add.sprite(50, 75 , 'missionbit');
+    game.physics.arcade.enable(this.player);
+    this.player.body.gravity.y = 1000;
+        
 
+        this.poops = game.add.group();
+        this.poops.enableBody = true;
+        this.poops.createMultiple(20, 'poop');
+        
+        var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        space.onDown.add(this.jump, this);
+        
+        game.time.events.loop(2000, this.addPoop, this);
+        
+    },
+   
     // update the state of the game
     update: function() {
-  
+        if (this.player.inWorld == false)
+            this.restartGame();
+
+
+        game.physics.arcade.overlap(this.player,this.poops, this.restartGame);
     },
   
     // makes the player jump
     jump: function() {
-  
+  this.player.body.velocity.y = -400;
     },
   
-    addPipe: function() {  
+    addPoop: function() { 
+        var poop = this.poops.getFirstDead();
+        
+        poop.reset(800, 400);
+        poop.body.velocity.x = -400;
+        poop.checkWorldBounds = true;
+        poop.outOfBoundsKill = true;
 
     },
 
     // resest the state of the game
     restartGame: function() {
-        game.state.start('default');    
+        game.state.start('default');
     }
 };
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', main);
+game.state.start('default')
